1d98d27c5a8ee4c2175fdb78adcdfd7c921b4ef1
jmoniatte <jmoniatte@fastmail.com>
2022-11-16 14:18:19 -0800

    Empty TracklibNext

--- ruby_tracklib2/lib/tracklib.rb
+++ ruby_tracklib2/lib/tracklib.rb
@@ -2,7 +2,7 @@ require "tracklib/version"
 require "rutie"
 
 module TracklibNext
-  unless defined?(TrackReader)
-    Rutie.new(:ruby_tracklib_next).init 'Init_Tracklib_Next', __dir__
-  end
+  # unless defined?(TrackReader)
+  #   Rutie.new(:ruby_tracklib_next).init 'Init_Tracklib_Next', __dir__
+  # end
 end
